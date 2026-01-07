022e1e6a1d12f1b20d2ba92d14a6e3b1101a1bf6
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-07 09:38:18 -0300

    variáveis

---
+++ aula 11/index.js
@@ -0,0 +1,5 @@
+let nome = 'João'; // Só VAMOS USAR LET
+var nome1 = 'João';
+let nome2; // Declara mas NÃO INICIALIZA -> Undefined
+console.log(nome, 'Nasceu');
+console.log('Em 2000', nome, 'Nasceu');
