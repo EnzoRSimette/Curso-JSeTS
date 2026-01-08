31443e9b8f9cab3f3f961e0a1c0cf7eb9dbe4d1f
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-08 09:59:54 -0300

    .

---
+++ aula 27/script.js
@@ -0,0 +1,11 @@
+let num1 = 9.54578;
+let num2 = Math.floor(num1);
+let num3 = Math.ceil(num1);
+let num4 = Math.round(num1); // se tem um .*5 ele arredonda para cima
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(Math.max(1,2,3,4,5,5,3,1,99,-10,-1,0-50,-21,-999999)); // Printa o maior número
+console.log(Math.min(1,2,3,4,5,5,3,1,99,-10,-1,0-50,-21,-999999)); // Prunta o menor número
+console.log(Math.random()); // número aleatório entre 0 e 1
+console.log(Math.random() * (10-5) + 5); // Número aleatório entre 5 e 10 (da para arredondar também)
