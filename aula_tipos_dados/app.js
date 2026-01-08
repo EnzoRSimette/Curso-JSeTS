903a98d8702928cd48f150e25f20eeb662cc34e5
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-08 08:48:02 -0300

    new file:   aula_tipos_dados/app.js

---
+++ aula_tipos_dados/app.js
@@ -0,0 +1,11 @@
+// Tipos de variáveis    
+    const nome1 = 'luiz'
+    const nome2 = "luiz"
+    const nome3 = `luiz`
+    const num1 = 1
+    const num2 = 1.5
+    let nomealuno; // undefined -> Não aponta para local algum na memória, não possui valor
+    let sobrenomealuno = null // Nulo -> Não aponta para local algum na memória, possui valor nulo
+let sim = true
+let nao = false
+console.log(typeof nome)
