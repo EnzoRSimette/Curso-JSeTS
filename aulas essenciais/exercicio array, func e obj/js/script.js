5ad633df40ae7739fe1a865188dce5c468e20e91
Enzo Ryan Simette <enzoryansimetteplr@gmail.com>
2026-01-08 14:14:26 -0300

    exercicios array, func e obj

---
+++ aulas essenciais/exercicio array, func e obj/js/script.js
@@ -0,0 +1,28 @@
+const pessoas = []; // Nosso array
+function nome() {
+    const form = document.querySelector(".form"); // SEMPRE USA ISSO, É IGUAL CSS PARA CLASSE, PARA ID É #***
+    const resultado = document.querySelector('.resultado');
+    // form.onsubmit = function (event) {
+    //     event.preventDefault();
+    //     alert(1);
+    // };   Jeito antigo
+    // Jeito moderno
+    function recebeEventoForm (evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const altura = form.querySelector('.altura');
+        const peso = form.querySelector('.peso');
+        pessoa = {
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+        peso: peso.value,
+        altura: altura.value,
+        };
+        pessoas.push(pessoa);
+        console.log(pessoas);
+        resultado.innerHTML += `<p>${nome.value}<br>${sobrenome.value}<br>${altura.value}<br>${peso.value}</p>`
+    }
+    form.addEventListener('submit', recebeEventoForm);
+  };
+nome();
